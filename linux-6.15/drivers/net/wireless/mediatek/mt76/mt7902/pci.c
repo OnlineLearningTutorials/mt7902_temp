fed35b8e998c3816f865b53a3c5068b22aeefa80
OnlineLearningTutorials <kush.kulshrestha.5@gmail.com>
2025-07-14 21:42:47 +0530

    Successful compilation of mt7902

--- linux-6.15/drivers/net/wireless/mediatek/mt76/mt7902/pci.c
+++ linux-6.15/drivers/net/wireless/mediatek/mt76/mt7902/pci.c
@@ -14,19 +14,10 @@
 #include "mcu.h"
 
 static const struct pci_device_id mt7902_pci_device_table[] = {
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7961),
+	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7902),
 		.driver_data = (kernel_ulong_t)MT7902_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7922),
-		.driver_data = (kernel_ulong_t)MT7922_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_ITTIM, 0x7922),
-		.driver_data = (kernel_ulong_t)MT7922_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x0608),
-		.driver_data = (kernel_ulong_t)MT7902_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x0616),
-		.driver_data = (kernel_ulong_t)MT7922_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7920),
-		.driver_data = (kernel_ulong_t)MT7920_FIRMWARE_WM },
-	{ },
+	{},
+
 };
 
 static bool mt7902_disable_aspm;
@@ -573,12 +564,10 @@ static struct pci_driver mt7902_pci_driver = {
 module_pci_driver(mt7902_pci_driver);
 
 MODULE_DEVICE_TABLE(pci, mt7902_pci_device_table);
-MODULE_FIRMWARE(MT7920_FIRMWARE_WM);
-MODULE_FIRMWARE(MT7920_ROM_PATCH);
+
 MODULE_FIRMWARE(MT7902_FIRMWARE_WM);
 MODULE_FIRMWARE(MT7902_ROM_PATCH);
-MODULE_FIRMWARE(MT7922_FIRMWARE_WM);
-MODULE_FIRMWARE(MT7922_ROM_PATCH);
+
 MODULE_AUTHOR("Sean Wang <sean.wang@mediatek.com>");
 MODULE_AUTHOR("Lorenzo Bianconi <lorenzo@kernel.org>");
 MODULE_DESCRIPTION("MediaTek MT7902E (PCIe) wireless driver");
